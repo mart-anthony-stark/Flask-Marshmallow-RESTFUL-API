8c9b41331f1aaaf8430132038843c5548251546a
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-25 22:27:45 +0800

    Add separate models file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+from models import Product, db
 
 # Init app
 app = Flask(__name__)
@@ -11,29 +12,15 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 #Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Init DB
-db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
 
-# Product model
-class Product(db.model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), unique=True)
-    description = db.Column(db.String(200))
-    price = db.Column(db.Float)
-    qty = db.Column(db.Integer)
-
-    def __init__(self, name, description, price, qty):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.qty = qty
 
 
 @app.route('/', methods=['GET'])
 def get():
-    return jsonify({'msg':"Hello world"})
+    products = Product.query.all()
+    return jsonify(products)
 
 # Run server
 if __name__ == '__main__':
---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Init DB
+db = SQLAlchemy()
+
+# Product model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
