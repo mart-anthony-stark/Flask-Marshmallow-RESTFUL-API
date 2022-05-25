4097b060ca6833c89806b6783bf042d4ed08405e
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-25 21:58:03 +0800

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+# Init app
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify({'msg':"Hello world"})
+
+# Run server
+if __name__ == '__main__':
+    app.run(debug=True)
