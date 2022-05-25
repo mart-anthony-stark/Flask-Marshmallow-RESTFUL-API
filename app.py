58b2b289ff314a8fb4c0dd2f66c1a7e863ecba49
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-25 22:06:30 +0800

    init app

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -5,6 +6,14 @@ import os
 
 # Init app
 app = Flask(__name__)
+base_dir = os.path.abspath(os.path.dirname(__file__))
+#Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_dir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init DB
+db = SQLAlchemy(app)
+# Init ma
+ma = Marshmallow(app)
 
 @app.route('/', methods=['GET'])
 def get():
