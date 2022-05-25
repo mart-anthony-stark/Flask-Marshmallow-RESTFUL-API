4e2040020af036ea544daf183d63d7cade08d7d1
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-25 22:10:23 +0800

    Add: product model

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from enum import unique
 from pickle import FALSE
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
@@ -14,6 +15,21 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # Init ma
 ma = Marshmallow(app)
+
+# Product model
+class Product(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.qty = qty
+
 
 @app.route('/', methods=['GET'])
 def get():
